d1ad004361c2d17ce83ebcc34be55fa0061e6a89
LeonardoRomulo <leo.romulo15@gmail.com>
2025-02-27 15:35:54 -0300

    Java script finalizado - conferir o porque a api não está trocando o icone do estado das nuvens

---
+++ index.js
@@ -0,0 +1,26 @@
+// constante que salva a chave da api
+const key = "1b5fd2813eab23c5a2e3c128680464bc";
+
+// Função que pega as informações da cidade na api e chama a função que retornará os valores solicitados para a interface do usuário
+function populateScreen(data){
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperatura").innerHTML =  (data.main.temp.toFixed(1)) +" ºC";
+    document.querySelector(".temp-maxima").innerHTML =  (data.main.temp_max.toFixed(1)) +" ºC";
+    document.querySelector(".temp-minima").innerHTML =  (data.main.temp_min.toFixed(1)) +" ºC";
+    document.querySelector(".tempo").innerHTML = (data.weather[0].description);
+    document.querySelector(".umidade").innerHTML = "Umidade: " +parseInt(data.main.humidity) + "%";
+    document.querySelector(".icone-tempo").src=`https://openweathermap.org/img/wn${data.weather[0].icon}/.png`; // Há um bloqueio que não está retornando o icone e alterando a imagem na interface de acordo com aos estados da nuvem Ex.: se estiver parcialmente nublado  tem que trocar o icone para a imagem correspondente.
+}
+
+// Aqui temos uma função que pega as informações da cidade na api e chama a função que retornará os valores solicitados para a interface do usuário
+async function searchCity (city) {
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then( response => response.json()); // integração da api
+    console.log(data);
+    populateScreen(data);
+}
+
+//Aqui ao clicar no botão a função search é chamada e chmará a função search city
+function search(){
+    const city = document.querySelector(".input-cidade").value;
+    searchCity(city);
+}
